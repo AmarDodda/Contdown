4ec3cc8ac870f8fcfc1cd7955adc2be3501be737
AmarDodda <amardodda23@gmail.com>
2024-04-05 17:06:41 +0530

    Countdown to Happy Independence Day

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function countdown(num, callback) {
-    setTimeout(function () {
+    setTimeout(() => {
       document.getElementById("countdown").innerText = num;
       callback(num - 1);
     }, 1000);
   }
 
-  countdown(10, function (num) {
-    countdown(num, function (num) {
-      countdown(num, function (num) {
-        countdown(num, function (num) {
-          countdown(num, function (num) {
-            countdown(num, function (num) {
-              countdown(num, function (num) {
-                countdown(num, function (num) {
-                  countdown(num, function (num) {
-                    countdown(num, function (num) {
-                      countdown(num, function () {
-                        document.getElementById("countdown").innerText =
-                          "HAPPY INDEPENDENCE DAY";
+  countdown(10, (num) => {
+    countdown(num, (num) =>{
+      countdown(num, (num)=> {
+        countdown(num,  (num) =>{
+          countdown(num,  (num)=> {
+            countdown(num, (num)=> {
+              countdown(num,  (num) =>{
+                countdown(num,  (num) =>{
+                  countdown(num,  (num) =>{
+                    countdown(num, (num) =>{
+                      countdown(num,  () =>{
+                        document.getElementById("countdown").innerText ="HAPPY INDEPENDENCE DAY";
                       });
                     });
                   });
